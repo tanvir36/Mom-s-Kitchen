e217f55403130c03e100c5c60725dd1757180fd3
TANVIR KAUR <tanvirkaur1804@gmail.com>
2021-06-17 10:00:47 -0400

    added the bcrypt library and cookie session not showing up on front end

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -5,24 +5,30 @@ class Login extends Component{
     state ={
         login: "",
     };
-
+   
     loginHandler =(event)=>{
         event.preventDefault();
         const username=event.target.name.value;
         const password= event.target.password.value;
         // console.log(`${username}   ${password}`);
+        axios.defaults.withCredentials= true;
         axios.post("http://localhost:8080/login",{username, password}).then((response)=>{
-                if(response.data.length<1){
-                        this.setState({
-                            login:"Wrong Username or Password" 
-                        });
-                } else{
-                    this.setState({
-                        login: (`Welcome ${response.data[0].userName}`)
-                    })
-                }   
+            console.log(response);
+                // if(response.data.length<1){
+                //         this.setState({
+                //                // login:"Wrong Username or Password" 
+                //             login: response.data.message
+                //         });
+                // } else{
+                //     this.setState({
+                //         // login: (`Welcome ${response.data[0].userName}`)
+                //         login: response.data.message
+                //     })
+                // }
+                this.setState({
+                    login: response.data.message
+                })
         })
-    
         event.target.reset();
     }
 
@@ -36,13 +42,21 @@ class Login extends Component{
         });
         event.target.reset();
     }
+    componentDidMount () {
+        axios.defaults.withCredentials= true;
+        axios.get("http://localhost:8080/login").then((response)=>{
+            console.log(response);
+        })
+    }
 
 
     render(){
+    
         return (
             <section>
                 <h1>Login Form </h1>
-                {this.state.login ? (<h2>{this.state.login}</h2>) : (<div></div>)}
+             {this.state.login===""?   (<h2>{this.state.login}</h2>):(<h1></h1>)}
+                 
                 <form onSubmit={this.loginHandler}>
                     <input type="text" id="name" placeholder="username"/>
                     <input type="password" id ="password" placeholder="password"/>
@@ -57,6 +71,7 @@ class Login extends Component{
 
             </section>
         )
+        
     }
 }
 export default Login;
--- server/index.js
+++ server/index.js
@@ -4,12 +4,40 @@ const cors = require('cors');
 const mysql= require('mysql');
 const kitchens = require('./routes/kitchens');
 const offers = require ('./routes/subscriptions');
+const bcrypt = require('bcrypt');
+const bodyParse = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const saltRounds =10;
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+));
+
+app.use(cookieParser());
+app.use(bodyParse.urlencoded({extended: true}));
+app.use(session({
+    key: "userId",
+    secret: "tanu",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        expire: 60*60*24,
+    },
+})
+);
+
 
 app.use (express.static('public'));
 app.use('/kitchens',kitchens);
 app.use('/kitchens',offers);
+
+
 
 const db= mysql.createConnection({
     user: 'root',
@@ -21,28 +49,49 @@ const db= mysql.createConnection({
 app.post('/register',(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    
-    db.query(
-        "INSERT INTO users (userName, password) VALUES (?,?)",
-    [username , password],
-    (err, result)=>{
-        console.log(err);
+
+    bcrypt.hash(password,saltRounds, (err, hash)=>{
+        if(err){
+            console.log(err);
+        }
+        db.query(
+            "INSERT INTO users (userName, password) VALUES (?,?)",
+        [username , hash],
+        (err, result)=>{
+            console.log(err);
+        }
+        );
+    })   
+})
+
+app.get('/login',(req,res)=>{
+    if(req.session.user){
+        res.send({loggedIn: true , user: req.session.user})
+    }else{  
+        res.send({loggedIn: false})
     }
-    );
 })
 
 app.post('/login',(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("SELECT * FROM users WHERE userName =? AND password=?",
-        [username , password],
+    db.query("SELECT * FROM users WHERE userName =?;",
+        username,
         (err,result)=>{
             if(err){
                 console.log(err);
             }
-            if(result){
-                res.send(result)
+            if(result.length>0){
+                bcrypt.compare(password, result[0].password,(error,response)=>{
+                    if(response){
+                        req.session.user =result;
+                        console.log(req.session.user);
+                        res.send(result);
+                    } else{
+                        res.send({Message: "Wrong Password Entered"});
+                    }
+                })          
             }else{
                 res.send({Message: "User Not found"});
             }
