a89f2752c116e3bc7d33d74523818d98980ec8d9
TANVIR KAUR <tanvirkaur1804@gmail.com>
2021-06-23 03:28:23 -0400

    things are not making sense so added terms and conditions in offers.js

--- client/src/Pages/Offers/Offers.js
+++ client/src/Pages/Offers/Offers.js
@@ -53,11 +53,20 @@ class  Offers extends Component {
                                 <p className="offers__card--price">{`Price: ${offer.price}`}</p>
                             </div>
                             <Link to ={`/${this.state.id}/${offer.name}/subscribe`}>          
-                                <button  className={`offers__subscribe num${i}`}>SUBSCRIBE</button> 
+                                <button  className={`offers__subscribe num${i}`}>ORDER</button> 
                             </Link>
                         </div>
-                    ))}                                                      
+                    ))}                                                                      
                </div>  
+               <div className="terms">
+                        <h1 className="terms__heading">Terms and Conditions</h1>
+                        <p className="terms__text">* Money would be deducted after order is delievered</p>
+                        <p className="terms__text">* You can call us at +16475400116 To customize the order.</p>
+                        <p className="terms__text">* We do provide the Tiffin service. Contact us for more information</p>
+                        <p className="terms__text">* We change our menu daily. Please keep checking our menu for new and exciting dishes</p>
+                        <p className="terms__text">* If you like food please refer us to your family and friends</p>
+                        <p className="terms__text">* Let us know how we can make service better. Any suggestions would be appreciated.</p>
+                    </div> 
                {/* {this.state.offers.map((offer,i)=>(
                     <div key={i}>  
                         <h1>{offer.description}</h1>
